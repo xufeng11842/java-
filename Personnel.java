6e4f1f9aa9e86658c908a1e7bdd9b0889031097f
xufeng11842 <72554068+xufeng11842@users.noreply.github.com>
2020-10-31 20:29:49 +0800

    Add files via upload

---
+++ Personnel.java
@@ -0,0 +1,12 @@
+package shiyan2;
+
+public class Personnel {    //父类
+    int number;
+    String name;
+    char sex;
+    public Personnel(int number, String name, char sex) {
+        this.number = number;
+        this.name = name;
+        this.sex = sex;
+    }
+}
